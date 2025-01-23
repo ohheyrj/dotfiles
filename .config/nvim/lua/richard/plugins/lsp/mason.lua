ce424f33d69a92dfb6a6027daf8d6044d7c2f654
Richard Annand <richard+git@ohheyrj.co.uk>
2025-01-23 01:47:45 +0000

    feat: added nvim config

---
+++ .config/nvim/lua/richard/plugins/lsp/mason.lua
@@ -0,0 +1,47 @@
+return {
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
+
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"ts_ls",
+				"html",
+				"cssls",
+				"lua_ls",
+				"pyright",
+				"terraformls",
+				"nil_ls",
+			},
+		})
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier",
+				"stylua",
+				"isort",
+				"black",
+				"tflint",
+				"tfsec",
+				"pylint",
+				"eslint_d",
+				"nixpkgs-fmt",
+				"gitleaks",
+			},
+		})
+	end,
+}
